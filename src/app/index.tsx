244dcdd31c7ad7bc2264c5505655b3cd1cde7b78
Adryel da Costa Lopes <adrielc.lopes@gmail.com>
2025-04-05 17:12:14 -0300

    adicionando Style

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,28 @@
-import {View, Text } from "react-native"
+import {View, Text, StyleSheet} from "react-native"
+import { red } from "react-native-reanimated/lib/typescript/Colors";
 
 export default function Index (){
     return (
         <View>
-            <Text>olá mundo!</Text>
-            <Text>Adryel</Text>
-            <Text>Endereço</Text>
+            <Text style={{ color: "blue", fontSize: 22 }}>olá mundo!</Text>
+            <Text style={styles.title} >Adryel</Text>
+            <Text style={styles.subTitle}>Endereço</Text>
+            <Text style={styles.contP}></Text>
         </View>
     );
 } 
+const styles = StyleSheet.create({
+    title: {
+        color: "red",
+        fontSize: 50,
+    },
+    subTitle:{
+        color: "green",
+        fontSize: 22,
+        fontFamily: "Times New Roman",
+    },
+    contP:{
+        color:"navy",
+        fontSize: 10,
+    }
+})
